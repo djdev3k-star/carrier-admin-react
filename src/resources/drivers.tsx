d9a4f8af5b78c1621f48eabe3ce6a9aadd9af3c0
Derrick James <186434410+djdev3k-star@users.noreply.github.com>
2025-07-23 21:38:58 -0500

    Updated drivers.tsx

--- src/resources/drivers.tsx
+++ src/resources/drivers.tsx
@@ -2,34 +2,46 @@
 import React from "react";
 import { List, Datagrid, TextField, EditButton, Edit, SimpleForm, TextInput, Create } from "react-admin";
 
-export const DriverList = () => (
-    <List>
+export const DriverList = (props) => (
+    <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="name" />
-            <TextField source="licenseNumber" />
+            <TextField source="license_number" />
+            <DateField source="license_expiry" />
             <TextField source="status" />
             <EditButton />
         </Datagrid>
     </List>
 );
 
-export const DriverEdit = () => (
-    <Edit>
+export const DriverEdit = (props) => (
+    <Edit {...props}>
         <SimpleForm>
+            <TextInput disabled source="id" />
             <TextInput source="name" />
-            <TextInput source="licenseNumber" />
-            <TextInput source="status" />
+            <TextInput source="license_number" />
+            <DateInput source="license_expiry" />
+            <SelectInput source="status" choices={[
+                { id: 'active', name: 'Active' },
+                { id: 'suspended', name: 'Suspended' },
+                { id: 'on_leave', name: 'On Leave' },
+            ]} />
         </SimpleForm>
     </Edit>
 );
 
-export const DriverCreate = () => (
-    <Create>
+export const DriverCreate = (props) => (
+    <Create {...props}>
         <SimpleForm>
             <TextInput source="name" />
-            <TextInput source="licenseNumber" />
-            <TextInput source="status" />
+            <TextInput source="license_number" />
+            <DateInput source="license_expiry" />
+            <SelectInput source="status" choices={[
+                { id: 'active', name: 'Active' },
+                { id: 'suspended', name: 'Suspended' },
+                { id: 'on_leave', name: 'On Leave' },
+            ]} />
         </SimpleForm>
     </Create>
 );
